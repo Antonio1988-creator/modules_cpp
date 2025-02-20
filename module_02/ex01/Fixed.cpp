bed6054e09329eff7078a33ad7182717930959eb
antonio <antonioguil1988@gmail.com>
2025-02-20 18:28:18 +0100

    finish, but with errors

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -36,6 +36,81 @@ Fixed&	Fixed::operator=(const Fixed& other)
 	return (*this);
 }
 
+bool	Fixed::operator>(const Fixed& other) const {
+	return (this->getRawBits() > other.getRawBits());
+}
+
+bool	Fixed::operator<(const Fixed& other) const {
+	return (this->getRawBits() < other.getRawBits());
+}
+
+bool	Fixed::operator>=(const Fixed& other) const {
+	return (this->getRawBits() >= other.getRawBits());
+}
+
+bool	Fixed::operator<=(const Fixed& other) const {
+	return (this->getRawBits() <= other.getRawBits());
+}
+
+bool	Fixed::operator==(const Fixed& other) const {
+	return (this->getRawBits() == other.getRawBits());
+}
+
+bool	Fixed::operator!=(const Fixed& other) const {
+	return (this->getRawBits() != other.getRawBits());
+}
+
+Fixed	Fixed::operator+(const Fixed& other) const {
+	Fixed	result;
+	
+	result.setRawBits(this->getRawBits() + other.getRawBits());
+	return (result);
+}
+
+Fixed	Fixed::operator-(const Fixed& other) const {
+	Fixed	result;
+
+	result.setRawbits(this->getRawBits() - other.getRawBits());
+	return (result);
+}
+
+Fixed	Fixed:: operator*(const Fixed& other) const {
+	Fixed	result;
+
+	result.setRawBits((this->getRawbits() * other.getRawBits()) >> Fixed::fractBits);
+	return (result);
+}
+
+Fixed	Fixed::operator/(const Fixed& other) const {
+	if (other.getRawBits() == 0)
+		throw std::logic_error("Division by zero!");
+	return (Fixed(this->toFloat() / other.toFloat());
+}
+
+Fixed&	Fixed::operator++(void) {
+	this->setRawBits(this->getRawBits() + 1);
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int) {
+	Fixed	curr(*this);
+
+	this->setRawBits(this->getRawBits() + 1);
+	return (curr);
+}
+
+Fixed&	Fixed::operator--(void) {
+	this->setRawBits(this->getRawBits() - 1);
+	return (*this);
+}
+
+Fixed	Fixed::operator--(int) {
+	Fixed	curr(*this);
+
+	this->setRawBits(this->getRawBits() - 1);
+	return (curr);
+}
+
 std::ostream&	operator<<(std::ostream& out, const Fixed& fixed)
 {
 	out << fixed.toFloat();
@@ -60,4 +135,28 @@ int 	Fixed::toInt() const
 float	Fixed::toFloat() const
 {
 	return ((float)this->value / (float)(1 << Fixed::fractBits));
+}
+
+Fixed&	Fixed::min(Fixed& x, Fixed& y) {
+	if (x < y)
+		return (x);
+	return (y);
+}
+
+Fixed&	Fixed::max(Fixed& x, Fixed& y) {
+	if (x > y)
+		return (x);
+	return (y);
+}
+
+const	Fixed&	Fixed::min(const Fixed& x, const Fixed& y) {
+	if (x < y)
+		return (x);
+	return (y);
+}
+
+const	Fixed&	Fixed::max(const Fixed& x, const Fixed& y) {
+	if (x > y)
+		return (x);
+	return (y);
 }
